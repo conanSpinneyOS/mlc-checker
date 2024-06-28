9c5615be0a132e1488273fd51dec0739a0699d9c
Conan Spinney <conanspinney@oceanskies.com>
2024-06-28 11:05:25 +0100

    Add icons

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,27 @@
+import Link from "next/link";
+
 const Header = () => {
+
+
 
     return (
         <div className="navbar bg-base-100 rounded-2xl shadow-2xl">
             <div className="navbar-start">
                 <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                     <div className="w-20 rounded-full">
-                        <img alt="Oceanskies Logo" src="/oslogo.png" />
+                        <Link href={"https://oceanskies.com/"} passHref={true}>  <img alt="Oceanskies Logo" src="/oslogo.png" /></Link>
                     </div>
                 </div>
             </div>
             <div className="navbar-center">
-              
+
             </div>
             <div className="navbar-end">
-                <button className="btn btn-ghost btn-circle">
+                {/* <button className="btn btn-ghost btn-circle">
                     <div className="indicator">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
                     </div>
-                </button>
+                </button> */}
             </div>
         </div>
     )
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -8,13 +8,13 @@ const Sidebar = ({ isOpen, onClose, handleInputChange, formState, handleReset })
             {/* Close button */}
             <div className="absolute top-2 right-10 mt-4 space-x-3 align-baseline">
                 <button onClick={handleReset} className="">
-                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5 h-5" stroke="currentColor"><path d="M125.7 160H176c17.7 0 32 14.3 32 32s-14.3 32-32 32H48c-17.7 0-32-14.3-32-32V64c0-17.7 14.3-32 32-32s32 14.3 32 32v51.2L97.6 97.6c87.5-87.5 229.3-87.5 316.8 0s87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3s-163.8-62.5-226.3 0L125.7 160z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5 h-5" stroke="currentColor"><path d="M125.7 160H176c17.7 0 32 14.3 32 32s-14.3 32-32 32H48c-17.7 0-32-14.3-32-32V64c0-17.7 14.3-32 32-32s32 14.3 32 32v51.2L97.6 97.6c87.5-87.5 229.3-87.5 316.8 0s87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3s-163.8-62.5-226.3 0L125.7 160z" /></svg>
                 </button>
                 <button
                     onClick={onClose}
                     className="py-2"
                 >
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" className="w-6 h-6" stroke="currentColor"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" className="w-6 h-6" stroke="currentColor"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" /></svg>
                 </button>
             </div>
             {/* Scrollable content */}
@@ -53,19 +53,8 @@ const Sidebar = ({ isOpen, onClose, handleInputChange, formState, handleReset })
                                 <option value="0">Cayman</option>
                             </select>
                         </div>
+
                         <div className="flex flex-col space-y-3">
-                        <label htmlFor="vesselKw">Vessel Engine Power (Kw)</label>
-                            <input
-                                type="range"
-                                name="vesselKw"
-                                id="vesselKw"
-                                min="0"
-                                max="6001"
-                                value={formState.vesselKw}
-                                className="range"
-                                onChange={handleInputChange}
-                            />
-                            <span>{formState.vesselKw > 6000 ? ("6000+") : (formState.vesselKw)} KW</span>
                             <label htmlFor="vesselGT">Vessel Tonnage (Gross Tonnage)</label>
                             <input
                                 type="range"
@@ -78,6 +67,19 @@ const Sidebar = ({ isOpen, onClose, handleInputChange, formState, handleReset })
                                 onChange={handleInputChange}
                             />
                             <span>{formState.vesselGT > 500 ? ("500+") : (formState.vesselGT)} GT</span>
+                            <label htmlFor="vesselKw">Vessel Engine Power (Kw)</label>
+                            <input
+                                type="range"
+                                name="vesselKw"
+                                id="vesselKw"
+                                min="0"
+                                max="6001"
+                                value={formState.vesselKw}
+                                className="range"
+                                onChange={handleInputChange}
+                            />
+                            <span>{formState.vesselKw > 6000 ? ("6000+") : (formState.vesselKw)} KW</span>
+
                             <label htmlFor="safehaven">Distance from safehaven (Miles)</label>
                             <input
                                 type="range"
